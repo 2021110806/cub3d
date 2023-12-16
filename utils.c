8b6c67ca14404f917634ecfba51736fd6de24af4
2021110806 <2021110806@dgu.ac.kr>
2023-12-16 21:41:10 +0900

    [12/16] : 천장, 바닥 래이트레이싱 일부

---
+++ utils.c
@@ -0,0 +1,36 @@
+#include "cub3d.h"
+
+int	*ft_int_malloc(int size)
+{
+	int	*return_value;
+
+	return_value = malloc (sizeof(int) * size);
+	if (!return_value)
+		exit(-1);
+	return (return_value);
+}
+
+int **ft_int_pointer_malloc(int size)
+{
+	int	**return_value;
+
+	return_value = malloc (sizeof(int *) * size);
+	if (!return_value)
+		exit(-1);
+	return (return_value);
+}
+
+void	init_texture(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < TEX_HEGIHT)
+	{
+		j = 0;
+		while (j < TEX_WIDTH)
+			data -> texture[i][j++] = 0;
+		i++;
+	}
+}
