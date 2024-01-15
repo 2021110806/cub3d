f7c333a1a5ca7209fd25d03a62fba18e5b5cd2f2
minkyole <minkyole@student.42seuol.kr>
2024-01-15 19:14:23 +0900

    feat : 벽 충돌 구현

--- bonus/move_command_bonus.c
+++ bonus/move_command_bonus.c
@@ -12,58 +12,84 @@
 
 #include "cub3d_bonus.h"
 
+int	check_movable(t_data *data, double move_x, double move_y)
+{
+	double	user_size_arr[2];
+	int		index;
+	int		result_x;
+	int		result_y;
+
+	user_size_arr[0] = USER_SIZE;
+	user_size_arr[1] = -USER_SIZE;
+	index = 0;
+	while (index < 2)
+	{
+		result_x = move_x + user_size_arr[index];
+		result_y = move_y + user_size_arr[index];
+		if (data->args.map.map[result_y][(int)move_x] == MINIMAP_CLOSE_DOOR || \
+		data->args.map.map[result_y][(int)move_x] == MINIMAP_WALL || \
+		data->args.map.map[result_y][(int)move_x] == MINIMAP_NULL || \
+		data->args.map.map[(int)move_y][result_x] == MINIMAP_CLOSE_DOOR || \
+		data->args.map.map[(int)move_y][result_x] == MINIMAP_WALL || \
+		data->args.map.map[(int)move_y][result_x] == MINIMAP_NULL)
+			return (FALSE);
+		index++;
+	}
+	return (TRUE);
+}
+
 void	move_up(t_data *data)
 {
-	if (data->args.map.map[(int)(data->player_position_y + \
-	data->player_view_direction_y * data->move_speed)] \
-	[(int)(data->player_position_x)] != MINIMAP_WALL)
-			data->player_position_y += \
-			data->player_view_direction_y * data->move_speed;
-	if (data->args.map.map[(int)(data->player_position_y)] \
-	[(int)(data->player_position_x + data->player_view_direction_x * \
-	data->move_speed)] != MINIMAP_WALL)
-			data->player_position_x += data->player_view_direction_x * \
-			data->move_speed;
+	double	move_x;
+	double	move_y;
+
+	move_x = data->player_position_x + data->player_view_direction_x * data->move_speed;
+	move_y = data->player_position_y + data->player_view_direction_y * data->move_speed;
+	if (check_movable(data, move_x, move_y))
+	{
+		data->player_position_x = move_x;
+		data->player_position_y = move_y;
+	}
 }
 
 void	move_down(t_data *data)
 {
-	if (data->args.map.map[(int)(data->player_position_y - \
-	data->player_view_direction_y * data->move_speed)] \
-	[(int)(data->player_position_x)] != MINIMAP_WALL)
-		data->player_position_y -= \
-		data->player_view_direction_y * data->move_speed;
-	if (data->args.map.map[(int)(data->player_position_y)] \
-	[(int)(data->player_position_x - data->player_view_direction_x * \
-	data->move_speed)] != MINIMAP_WALL)
-		data->player_position_x -= \
-		data->player_view_direction_x * data->move_speed;
+	double	move_x;
+	double	move_y;
+
+	move_x = data->player_position_x - data->player_view_direction_x * data->move_speed;
+	move_y = data->player_position_y - data->player_view_direction_y * data->move_speed;
+	if (check_movable(data, move_x, move_y))
+	{
+		data->player_position_x = move_x;
+		data->player_position_y = move_y;
+	}
 }
 
 void	move_left(t_data *data)
 {
-	if (data->args.map.map[(int)(data->player_position_y + \
-	data->camera_plane_y * data->move_speed)] \
-	[(int)(data->player_position_x)] != MINIMAP_WALL)
-			data->player_position_y += \
-			data->camera_plane_y * data->move_speed;
-	if (data->args.map.map[(int)(data->player_position_y)] \
-	[(int)(data->player_position_x + data->camera_plane_x * \
-	data->move_speed)] != MINIMAP_WALL)
-			data->player_position_x += data->camera_plane_x * \
-			data->move_speed;
+	double	move_x;
+	double	move_y;
+
+	move_x = data->player_position_x + data->camera_plane_x * data->move_speed;
+	move_y = data->player_position_y + data->camera_plane_y * data->move_speed;
+	if (check_movable(data, move_x, move_y))
+	{
+		data->player_position_x = move_x;
+		data->player_position_y = move_y;
+	}
 }
 
 void	move_right(t_data *data)
 {
-	if (data->args.map.map[(int)(data->player_position_y - \
-	data->camera_plane_y * data->move_speed)] \
-	[(int)(data->player_position_x)] != MINIMAP_WALL)
-		data->player_position_y -= \
-		data->camera_plane_y * data->move_speed;
-	if (data->args.map.map[(int)(data->player_position_y)] \
-	[(int)(data->player_position_x - data->camera_plane_x * \
-	data->move_speed)] != MINIMAP_WALL)
-		data->player_position_x -= \
-		data->camera_plane_x * data->move_speed;
+	double	move_x;
+	double	move_y;
+
+	move_x = data->player_position_x - data->camera_plane_x * data->move_speed;
+	move_y = data->player_position_y - data->camera_plane_y * data->move_speed;
+	if (check_movable(data, move_x, move_y))
+	{
+		data->player_position_x = move_x;
+		data->player_position_y = move_y;
+	}
 }
